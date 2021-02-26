1ee7dd2e569099f6337f73e0b1b6029eef71f732
Wojtek <voodoonio@gmail.com>
2021-02-26 23:44:09 +0100

    Windows Service Example

---
+++ 001_WindowsService/SimpleWindowsService/MyWindowsService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWindowsService
+{
+    public partial class MyWindowsService : ServiceBase
+    {
+        public MyWindowsService()
+        {
+            InitializeComponent();
+            this.ServiceName = "MyWindowsService";
+            this.EventLog.Source = "MyWindowsService";
+            this.EventLog.Log = "Application";
+
+            this.CanHandlePowerEvent = true;
+            this.CanHandleSessionChangeEvent = true;
+            this.CanPauseAndContinue = true;
+            this.CanShutdown = true;
+            this.CanStop = true;
+
+            if (!EventLog.SourceExists("MyWindowsService"))
+                EventLog.CreateEventSource("MyWindowsService", "Application");
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("C:\\WS.txt", false);
+            sw.WriteLine("Hello world");
+            sw.Flush();
+            sw.Close();
+        }
+
+        protected override void OnStop()
+        {
+            //do some action 
+        }
+    }
+}
---
+++ 001_WindowsService/SimpleWindowsService/MyWindowsServiceInstaller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace SimpleWindowsService
+{
+    [RunInstaller(true)]
+    public partial class MyWindowsServiceInstaller : System.Configuration.Install.Installer
+    {
+        public MyWindowsServiceInstaller()
+        {
+            InitializeComponent();
+
+            ServiceProcessInstaller serviceProcessInstaller = new ServiceProcessInstaller();
+            ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+            //# Service Account Information
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            serviceProcessInstaller.Username = null;
+            serviceProcessInstaller.Password = null;
+
+            //# Service Information
+            serviceInstaller.DisplayName = "MyWindowsService";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            // This must be identical to the WindowsService.ServiceBase name
+            // set in the constructor of WindowsService.cs
+            serviceInstaller.ServiceName = "MyWindowsService";
+
+            this.Installers.Add(serviceProcessInstaller);
+            this.Installers.Add(serviceInstaller);
+        }
+    }
+}
